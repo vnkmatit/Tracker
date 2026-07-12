ef69404e2e466fb5d76591cf4992bb721036a299
vnkmatit <matiocatio2020@gmail.com>
2026-07-12 17:33:27 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -36,9 +36,7 @@ if password_input == TRAINER_PASSWORD:
     
     # Fetch active member names for the dropdown list
     try:
-
-# By checking against a dynamic timestamp, the web request changes every 5 seconds, forcing a live sync
-response = supabase.table("clan_members").select("*").neq("username", f"cache_bypass_{time.time()}").execute()
+        response = supabase.table("clan_members").select("*").neq("username", f"cache_bypass_{time.time()}").execute()
         existing_users = [row['username'] for row in response.data]
     except Exception:
         existing_users = []
