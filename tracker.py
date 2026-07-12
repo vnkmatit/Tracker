3a144ac1d47563b64b9c6ad8263bfe2ac2f20f50
vnkmatit <matiocatio2020@gmail.com>
2026-07-12 18:36:40 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 
 # --- DATABASE CONNECTION ---
 SUPABASE_URL = "https://xvlipedpfyngtwgnrpzt.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bGlwZWRwZnluZ3R3Z25ycHp0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4Mzg1MjcwNCwiZXhwIjoyMDk5NDI4NzA0fQ.sbc4c1g37cCSxw6ReLOGjotIfs13PFyqumwxrMRgyWk"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bGlwZWRwZnluZ3R3Z25ycHp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTI3MDQsImV4cCI6MjA5OTQyODcwNH0.4bgMQTbhA5zCT9OAVdQBNCueXcrcAuoWELDnWF7Eci0"
 TRAINER_PASSWORD = "ClanTrainer2026" 
 
 @st.cache_resource
