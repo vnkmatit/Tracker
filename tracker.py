f9ec47e710a5bc20fa2273047a6b1c3b6db2901c
vnkmatit <matiocatio2020@gmail.com>
2026-07-13 17:55:48 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,20 +1,5 @@
-import streamlit as st
-from supabase import create_client
+import pandas as pd
 
-st.set_page_config(page_title="Test")
+df = pd.DataFrame(response.data)
 
-st.write("Step 1")
-
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-
-st.write("Step 2")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-st.write("Step 3")
-response = supabase.table("clan_members").select("*").execute()
-
-st.write("Step 4")
-st.write(response.data)
-
+st.write(df)
