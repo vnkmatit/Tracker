11a3da5a40d33b87f2546166cf2e42ac62524d57
vnkmatit <matiocatio2020@gmail.com>
2026-07-13 17:50:52 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -3,4 +3,13 @@ from supabase import create_client
 
 st.set_page_config(page_title="Test")
 
-st.write("Imported Supabase successfully")
+st.write("Step 1")
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+st.write("Step 2")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+st.write("Step 3")
